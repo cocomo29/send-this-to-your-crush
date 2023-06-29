966ed4b515f86337c01e942b62562edf683b244d
rkg2706 <138055767+rkg2706@users.noreply.github.com>
2023-06-29 06:53:36 +0000

    new Commit

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
 
  let p = document.createElement("p");
- p.innerText = "Congrats you made the right choice 🎉 \nDM me the time and venue 😉";
+ p.innerText = "Congrats you made the right choice 🎉 \n chlo DM me the time fata fat 😉";
   p.style.fontSize = "2rem";
   p.style.fontWeight = "bold";
   p.style.textAlign = "center";
